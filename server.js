7a45148ea2c31b0e76ead52c45292dbe7ec82b01
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-10-28 20:35:47 +0500

    CRUD - READ
    write Hello World back to the browser. We do so by
    using a send method that comes with the response object:

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ console.log('May Node be with you')
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
+
+//app.get(endpoint, callback)
+
+// We normally abbreviate `request` to `req` and `response` to `res`.
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
