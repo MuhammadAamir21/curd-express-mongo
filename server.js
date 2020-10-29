c8bfb866b72d99e2e4f971bcbdd96dc6a79159ea
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-10-30 01:56:47 +0500

    Connecting to mongoDB server

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://127.0.0.1:27017'
+
+const dbName = 'star-wars-quotes'
+let db
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+
+  // Storing a reference to the database so you can use it later
+  db = client.db(dbName)
+  console.log(`Connected MongoDB: ${url}`)
+  console.log(`Database: ${dbName}`)
+})
+
 //The urlencoded method within body-parser tells body-parser to extract
 //data from the <form> element and add them to the body property
 //in the request object.
