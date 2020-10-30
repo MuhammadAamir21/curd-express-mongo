c3e105079c10895bfb8a352a65cdeeb62a7babef
Muhammad Aamir <51251914+MuhammadAamir21@users.noreply.github.com>
2020-10-31 04:21:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient
+
+console.log('May Node be with you')
+
+
+
+app.listen(3000, function() {
+    console.log('http://localhost:3000/')
+})
+
 // ---------------------------------------------My code enjoying the weather----------------------------------------------------
+
+
 const connectionString = 'mongodb://127.0.0.1:27017'
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true
@@ -7,28 +23,33 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true
 
 console.log('Connected to Database')
 const db = client.db('HLF')
-const signupCollection = db.collection('Login')
+const signupCollection = db.collection('signup')
 
-app.get('/signup', async function (req, res) {
+app.get('/signups', async function (req, res) {
   
-  db.collection('signup').find().toArray()
+  signupCollection.find().toArray()
   .then(results => {
     return res.send(results)
    
   })
-  .catch(error => res.status(401).send(error))
+  .catch(error => res.send(error))
+
+  //.catch(error => res.status(400).send(error))
   
 });
 
-app.post('/signup', async function (req, res) {
-  
 
+app.post('/signup',  async function (req, res) {
+
+  console.log(req.body)
+  
   signupCollection.insertOne(req.body)
   .then(result => {
-    res.status(400).send()
+    
+    res.status(200).send(req.body)
    
   })
-  .catch(error => res.status(401).send(error))
+  .catch(error => res.send(error))
   
 });
 
